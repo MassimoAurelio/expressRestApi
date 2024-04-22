97a6d7c27e2df1f986e35321240dfde16846c09e
massimoaurelio <massimoaureliowork@gmail.com>
2024-04-22 17:01:28 +0300

    update

---
+++ controllers/productController.js
@@ -0,0 +1,62 @@
+const Product = require("../models/product.model");
+
+
+// Create product
+exports.createProduct = async (req, res) => {
+    try {
+      const product = await Product.create(req.body);
+      res.status(200).json(product);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  };
+  
+  // Find all products
+  exports.getAllProducts = async (req, res) => {
+    try {
+      const products = await Product.find({});
+      res.status(200).json(products);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  };
+  
+  // Find product by ID
+  exports.getProductById = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const product = await Product.findById(id);
+      res.status(200).json(product);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  };
+  
+  // Update product
+  exports.updateProduct = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const product = await Product.findByIdAndUpdate(id, req.body);
+      if (!product) {
+        return res.status(404).json({ message: "Product not found" });
+      }
+      const updatedProduct = await Product.findById(id);
+      res.status(200).json(updatedProduct);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  };
+  
+  // Delete product
+  exports.deleteProduct = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const product = await Product.findByIdAndDelete(id);
+      if (!product) {
+        return res.status(404).json({ message: "Product not found" });
+      }
+      res.status(200).json({ message: "Product deleted successfully" });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  };
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const Product = require("./models/product.model");
+const productRoutes = require("./routes/productRoutes");
 
 const app = express();
 app.use(express.json());
@@ -9,44 +9,18 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.post("/api/products", async (req, res) => {
-  try {
-    const product = await Product.create(req.body);
-    res.status(200).json(product);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-app.get("/api/products", async (req, res) => {
-  try {
-    const product = await Product.find({});
-    res.status(200).json(product);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-app.get("/api/products/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findById(id);
-    res.status(200).json(product);
-  } catch {
-    res.status(500).json({ message: error.message });
-  }
-});
+app.use("/api/products", productRoutes);
 
 mongoose
   .connect(
     "mongodb+srv://nynnwork:8QL2up5l8jJUpro5@restapi.hwpjh4s.mongodb.net/Node-API?retryWrites=true&w=majority&appName=restApi"
   )
   .then(() => {
-    console.log("CONNECT DB");
+    console.log("Connected to DB");
     app.listen(3000, () => {
-      console.log("HELLO WORLD");
+      console.log("Server is running on port 3000");
     });
   })
-  .catch(() => {
-    console.log("ERROR DB");
+  .catch((error) => {
+    console.log("Error connecting to DB:", error.message);
   });
